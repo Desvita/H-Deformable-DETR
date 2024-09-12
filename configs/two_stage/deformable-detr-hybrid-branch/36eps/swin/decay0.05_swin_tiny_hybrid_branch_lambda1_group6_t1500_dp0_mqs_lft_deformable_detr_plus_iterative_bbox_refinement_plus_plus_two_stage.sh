1d6e846dbbc2383fd40a090459a7d341d926cc76
Desvita <59883177+Desvita@users.noreply.github.com>
2024-09-12 21:00:50 +0800

    dim feedfoward and dropout

--- configs/two_stage/deformable-detr-hybrid-branch/36eps/swin/decay0.05_swin_tiny_hybrid_branch_lambda1_group6_t1500_dp0_mqs_lft_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh
+++ configs/two_stage/deformable-detr-hybrid-branch/36eps/swin/decay0.05_swin_tiny_hybrid_branch_lambda1_group6_t1500_dp0_mqs_lft_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh
@@ -9,14 +9,14 @@ python -u main.py \
     --output_dir ${EXP_DIR} \
     --with_box_refine \
     --two_stage \
-    --dim_feedforward 2048 \
+    --dim_feedforward 1024 \
     --epochs 36 \
     --lr_drop 30 \
     --num_queries_one2one 300 \
     --num_queries_one2many 1500 \
     --k_one2many 6 \
     --lambda_one2many 1.0 \
-    --dropout 0.0 \
+    --dropout 0.1 \
     --mixed_selection \
     --look_forward_twice \
     --backbone swin_tiny \
